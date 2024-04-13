6ddffbdbc9682ffff9d5185fc4744dd5b0ca8854
Marek Misiak <misiak.marek@gmail.com>
2024-04-13 19:40:56 +0200

    routing

--- tiu_mdm.client/src/main.jsx
+++ tiu_mdm.client/src/main.jsx
@@ -2,9 +2,36 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import { Article } from './Article/ArticlePage.jsx'
+import { Articles } from './Article/ArticlesPage.jsx'
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: "/article",
+        element: <Article />,
+    },
+    {
+        path: "/articles",
+        element: <Articles />,
+    },
+
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
