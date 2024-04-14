24e8be74ead5ff96345289f4e4e3cfeb5771a323
Marek Misiak <misiak.marek@gmail.com>
2024-04-14 08:30:17 +0200

    part 1 finished

--- tiu_mdm.client/src/Article/ArticlePage.jsx
+++ tiu_mdm.client/src/Article/ArticlePage.jsx
@@ -1,10 +1,22 @@
 import Button from 'react-bootstrap/Button';
-
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
 export const Article = () => {
     return (
        
         <div>
-            <Button variant="outline-primary">Primary</Button>{' '}
+            <div>
+                <Form>
+                    <Row>
+                        <Col>
+                            <Button variant="outline-primary">Save</Button>{' '}
+                            <Button variant="outline-primary">Cancel</Button>{' '}
+                        </Col>   
+                    </Row>
+                </Form>
+            </div>
+            
             
         </div>
     )
