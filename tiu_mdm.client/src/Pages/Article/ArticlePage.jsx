1ee3b479141a424808bb2d6531c0ebc8f8323b7a
Marek Misiak <misiak.marek@gmail.com>
2024-04-18 08:37:20 +0200

    Global tab

--- tiu_mdm.client/src/Pages/Article/ArticlePage.jsx
+++ tiu_mdm.client/src/Pages/Article/ArticlePage.jsx
@@ -7,14 +7,14 @@ import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs'
 export const Article = () => {
     return (
-       
+
         <Container fluid>
             <Form>
-                    <Row>
-                        <Col xs="auto" className="pb-3">
-                            <Button variant="outline-primary">Save</Button>{' '}
-                            <Button variant="outline-primary">Cancel</Button>{' '}
-                        </Col>   
+                <Row>
+                    <Col xs="auto" className="pb-3">
+                        <Button variant="outline-primary">Save</Button>{' '}
+                        <Button variant="outline-primary">Cancel</Button>{' '}
+                    </Col>
                 </Row>
                 <Row>
                     <Tabs
@@ -23,7 +23,51 @@ export const Article = () => {
                         className="mb-3"
                     >
                         <Tab eventKey="global" title="Global data">
-                            Tab content for global data
+                            <Form>
+                                <Form.Group className="mb-3" controlId="articleForm.Clasification">
+                                    <Form.Label>Clasification</Form.Label>
+                                    <Form.Select aria-label="Default select example">                                        
+                                        <option>Open this select menu</option>
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                    </Form.Select>
+                                </Form.Group>
+                                
+                                <Form.Group className="mb-3" controlId="articleForm.MdmId">
+                                    <Form.Label>MDM Item Id</Form.Label>
+                                    <Form.Control type="text" placeholder="1" disabled readOnly />
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="articleForm.BasicUom">
+                                    <Form.Label>Basic UOM</Form.Label>
+                                    <Form.Control type="text"  />
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="articleForm.ContentUom">
+                                    <Form.Label>Content UOM</Form.Label>
+                                    <Form.Control type="text" />
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="articleForm.Ean">
+                                    <Form.Label>EAN Number</Form.Label>
+                                    <Form.Control type="text"  />
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="articleForm.BrandName">
+                                    <Form.Label>Name of Brand</Form.Label>
+                                    <Form.Control type="text"   />
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="articleForm.BrandItemName">
+                                    <Form.Label>Brand Item Name</Form.Label>
+                                    <Form.Control type="text" />
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="articleForm.BrandItemId">
+                                    <Form.Label>Brand Item ID</Form.Label>
+                                    <Form.Control type="text" />
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="articleForm.ControlTextarea1">
+                                    <Form.Label>Example textarea</Form.Label>
+                                    <Form.Control as="textarea" rows={3} />
+                                </Form.Group>
+                            </Form>
+
                         </Tab>
                         <Tab eventKey="pl" title="PL data">
                             Tab content for Pl data
@@ -33,8 +77,8 @@ export const Article = () => {
                         </Tab>
                     </Tabs>
                 </Row>
-              </Form>
-                
+            </Form>
+
         </Container>
     )
 }
